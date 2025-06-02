0e31e0e580e17a43042e86bb2b3c5566cdb04892
Hamir Chauhan <hamirchauhan.45@gmail.com>
2025-06-02 21:15:44 +0530

    Updated API URL to live backend

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -10,7 +10,7 @@ const LoginPage = ({ setUser }) => {
     const credential = credentialResponse.credential;
 
     try {
-      const res = await fetch('http://localhost:3001/api/google', {
+      const res = await fetch('https://xeno-crm-backend-zlmh.onrender.com/api/google', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ credential }),
