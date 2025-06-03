eb43859ca34990e0f9758abae0810c8847b278fc
Hamir Chauhan <hamirchauhan.45@gmail.com>
2025-06-03 12:39:47 +0530

    Fix signup fetch URL to use deployed backend

--- src/Signup.js
+++ src/Signup.js
@@ -23,7 +23,7 @@ const Signup = () => {
     }
 
     try {
-      const response = await fetch('/api/signup', {
+      const response = await fetch('https://xeno-crm-backend-zlmh.onrender.com/api/signup', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
